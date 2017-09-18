0d287e324153976b35bd5b0e6eab6a47c6669dac
Mark Llobrera <mark@dirtystylus.com>
2017-09-18 13:23:47 -0400

    added single component, comments component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    // get the post
+    const post = this.props.posts[i];
+    // get the comments
+    const postComments = this.props.comments[postId] || [];
+    
     return (
       <div className="single-photo">
-        I’m the single!
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
